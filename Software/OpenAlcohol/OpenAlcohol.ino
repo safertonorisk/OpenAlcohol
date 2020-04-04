82e1018c51c0149e197d70546b9a89e204f1f335
Kristofer Sano <35412153+safertonorisk@users.noreply.github.com>
2020-04-05 00:21:15 +0800

    Update OpenAlcohol.ino

--- Software/OpenAlcohol/OpenAlcohol.ino
+++ Software/OpenAlcohol/OpenAlcohol.ino
@@ -1,5 +1,5 @@
 #include <NewPing.h>
-NewPing sens(8,7,20);
+NewPing sens(8,7,20); // sens(trig,echo,max_distance)
 void setup() {
   // put your setup code here, to run once:
   pinMode(2,OUTPUT);
@@ -8,6 +8,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int dist = sens.ping_cm();
+  
   if(dist<15&&dist!=0){
     digitalWrite(2,HIGH);
     delay(200);
